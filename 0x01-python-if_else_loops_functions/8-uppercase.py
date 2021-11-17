477baf044a3f3f0d7f38653eaf1dfd5dc1e1ccdd
sofology <36291768+sofology@users.noreply.github.com>
2021-11-17 13:46:26 +0300

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for letter in str:
-        if letter <= 'z' and letter >= 'a':
-            letter = chr(ord(letter) - 32)
-        print("{}".format(letter), end='')
-    print()
-    
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
