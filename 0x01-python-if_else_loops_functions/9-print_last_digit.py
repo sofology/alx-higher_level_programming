697d06327a7c824310a49fa6e9b5391068dab6f7
sofology <36291768+sofology@users.noreply.github.com>
2021-11-17 13:48:53 +0300

    if/else, loops, functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        number = -number
-    print("{:d}".format(number % 10), end='')
-    return number % 10
-    
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
