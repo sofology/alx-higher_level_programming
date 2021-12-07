2fdc9dc97cb33ead25516c98588fe1cee634c512
sofology <36291768+sofology@users.noreply.github.com>
2021-12-07 16:11:24 +0300

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-class Square:
-    """Create Class Empty"""
-    pass
-
-    def __init__(self, size):
-        """Create atribute class with init method"""
-        self.__size = size
