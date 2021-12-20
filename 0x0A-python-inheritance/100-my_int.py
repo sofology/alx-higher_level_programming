e68b3a1d1ba4f1ef38650bdedae267aa8f31e289
sofology <sofisintayhu19@gmail.com>
2021-12-20 08:47:57 -0500

    a class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""MyInt class inherts of int"""
+# 100-my_int.py
+
+"""Defines a class MyInt that inherits from int."""
 
 
 class MyInt(int):
-    """MyInt class"""
+    """Invert int operators == and !=."""
+
     def __eq__(self, value):
-        """Method for not equal"""
-        return False
+        """Override == opeartor with != behavior."""
+        return self.real != value
 
     def __ne__(self, value):
-        """Method not equal to equal"""
-        return True
+        """Override != operator with == behavior."""
+        return self.real == value
