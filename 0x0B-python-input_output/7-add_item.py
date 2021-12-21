e6f8d049c06f68a0a40e0f3ac43e3d30f1c1a517
sofology <36291768+sofology@users.noreply.github.com>
2021-12-21 17:38:08 +0300

    a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-# 7-add_item.py
-"""Add all arguments to a Python list and save them to a file."""
+
+"""module 7-add_item"""
+
+
 import sys
+import json
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+new_list = []
+
+try:
+    new_list = list(load_from_json_file('add_item.json'))
+except:
+    len(new_list) is 0
+
+for a in range(1, len(sys.argv)):
+    new_list.append(sys.argv[a])
+save_to_json_file(new_list, "add_item.json")
