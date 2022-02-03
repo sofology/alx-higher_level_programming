e4e8cd5cf007ff9c691f7dd728152439982f3a60
sofology <36291768+sofology@users.noreply.github.com>
2022-02-03 10:45:23 +0300

    a script that creates the MySQL server user user_0d_1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Script for create user MySQL server user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = PASSWORD('user_0d_1_pwd');
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
