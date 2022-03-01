4823dbf6ea71e38eecb699d0a760f4d716ef50f3
sofology <36291768+sofology@users.noreply.github.com>
2022-03-01 12:03:08 +0300

    Increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,11 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-
-myObject.incr=function () {
+myObject.incr = function () {
   this.value++;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
