6881e2be91999c142f9afa303e3ce5681fab1d6d
sofology <36291768+sofology@users.noreply.github.com>
2022-03-01 10:04:00 +0300

    3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-const myVar = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myVar);
+const txt1 = 'C is fun';
+const txt2 = 'Python is cool';
+const txt3 = 'JavaScript is amazing';
+console.log(txt1);
+console.log(txt2);
+console.log(txt3);
