6f1453b2bda8ab8404c9f847d5b917def5773b5e
sofology <36291768+sofology@users.noreply.github.com>
2022-03-18 14:36:53 +0300

    0x11. Python - Network #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 """Fetches https://alx-intranet.hbtn.io/status"""
 if __name__ == "__main__":
     import requests
-    req = requests.get('https://intranet.hbtn.io/status')
+    req = requests.get('https://alx-intranet.hbtn.io/status')
     response = req.text
     print("Body response:")
     print("\t- type: {}".format(type(response)))
