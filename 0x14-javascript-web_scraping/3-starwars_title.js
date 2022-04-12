6ad3151a804267d18df44367ac171c03c6a4054e
sofology <sofisintayehu19@gmail.com>
2022-04-12 17:13:35 +0300

    0x14 JavaScript - web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+// prints the title of a Star Wars movie
+// where the episode number matches a given integer
 
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request('http://swapi.co/api/films/:id' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  console.log(JSON.parse(body).title);
+request.get(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
