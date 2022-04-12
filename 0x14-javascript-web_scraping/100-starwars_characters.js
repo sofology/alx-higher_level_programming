737d159755949074fdb9b581a7d2250e3bc9706f
sofology <sofisintayehu19@gmail.com>
2022-04-12 17:21:02 +0300

    0x14 JavaScript - web scraping

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,17 +1,23 @@
 #!/usr/bin/node
+// prints all characters of a Star Wars movie
+// The first argument is the Movie ID
 
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
+request.get(url, (error, response, data) => {
+  if (error) console.log(error);
+  else {
+    const film = JSON.parse(data);
+    const characters = film.characters;
+    for (const character of characters) {
+      request(character, (error, response, data) => {
+        if (error) console.log(error);
+        else {
+          const chars = JSON.parse(data);
+          console.log(chars.name);
+        }
+      });
+    }
   }
-  JSON.parse(body).characters.forEach(function (url, callback) {
-    request(url, function (error, response, body) {
-      if (error) {
-        console.error(error);
-      }
-      console.log(JSON.parse(body).name);
-    });
-  });
 });
