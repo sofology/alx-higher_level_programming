6cb552dc9144494e69e23415829521f9406fa9cc
sofology <sofisintayehu19@gmail.com>
2022-04-12 17:28:00 +0300

    0x14 JavaScript - web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,50 +1,49 @@
 #!/usr/bin/node
-/**
- * Prints all characters of a Star Wars movie
- * The first argument is the Movie ID
- * in the same order of the list characters in the /films/ response
- */
 
- const request = require('request');
- const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
- /*
- request.get(url, (error, response, data) => {
-   if (error) console.log(error);
-   else {
-     const film = JSON.parse(data);
-     const characters = film.characters;
-     let idx;
-     for (idx = 0; idx < characters.length; idx++) {
-       request(characters[idx], (error, response, data) => {
-         if (error) console.log(error);
-         else {
-           const chars = JSON.parse(data);
-           console.log(chars.name);
-         }
-       });
-     }
-   }
- });
- */
- 
- request.get(url, (error, response, data) => {
-   if (error) console.log(error);
-   else {
-     const character = JSON.parse(data).characters;
-     printcur(character, 0);
-   }
- });
- 
- function printcur (arr, idx) {
-   if (idx >= arr.length) {
-     return;
-   }
-   request.get(arr[idx], (e, r, b) => {
-     if (e) console.log(e);
-     else {
-       console.log(JSON.parse(b).name);
-     }
-     return printcur(arr, idx + 1);
-   });
- }
- 
+const request = require('request');
+
+/*
+let characters = [];
+let dict = {};
+function addToDict (url, name) {
+  dict[url] = name;
+}
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  characters = JSON.parse(body).characters
+  characters.forEach(function (url) {
+    request(url, function (error, response, body) {
+      if (error) {
+        console.error(error);
+      }
+      addToDict(url, JSON.parse(body).name);
+    });
+  });
+  characters.forEach(function (item) {
+    console.log(dict[item]);
+  })
+});
+*/
+
+function helpRequest (arr, i) {
+  if (i === arr.length) {
+    return;
+  }
+  request(arr[i], function (error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    console.log(JSON.parse(body).name);
+    helpRequest(arr, i + 1);
+  });
+}
+
+request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const charac = JSON.parse(body).characters;
+  helpRequest(charac, 0);
+});
